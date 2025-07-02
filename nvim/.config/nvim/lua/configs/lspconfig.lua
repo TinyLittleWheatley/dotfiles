02a79f26e47b3b69be9fc24f5f1165c683eafd92
Mohammad Karami <1380mkkarami@gmail.com>
2025-07-02 11:07:12 +0330

    Update some lsp server confs

--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,12 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "clangd", "pyright" }
+local servers = { "clangd", "pyright", "zls" }
 vim.lsp.enable(servers)
+
+vim.lsp.config("pyright", {
+  root_dir = function(_)
+    return vim.loop.cwd()
+  end,
+})
 
 -- read :h vim.lsp.config for changing options of lsp servers
